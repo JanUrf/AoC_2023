395bc21ca662e08aabcb580d73b4dbbe0956fc4b
JanUrf <janurfei@gmail.com>
2023-12-03 14:16:05 +0100

    Add solution day3 part2

--- day3/GearRatios.py
+++ day3/GearRatios.py
@@ -8,9 +8,11 @@ from itertools import product, groupby
 #for debugging
 np.set_printoptions(threshold=sys.maxsize)
 
+#default shape
 input_shape = (140, 140)
 
 def calculate_neighbours(iy: int, ix: int)-> list:
+    #returns the indices of the neighbours of the searched cell.
     # crop the boarders
     if iy == 0:
         range_y = [iy, iy+1]
@@ -33,10 +35,14 @@ def parse_input(file_path: Path) -> npt.NDArray:
     with open(file_path, 'r') as f:
         for line in f:
             data.append([None if cell == '.' else cell for cell in line.rstrip()])
-            
-    return np.array(data)
+    data = np.array(data)
+    #set shape
+    input_shape = data.shape      
+    return data
             
 def create_mask(data: npt.NDArray)-> npt.NDArray:
+    # mark a mask matrix with cells that are next to a character and
+    # has to be potentially encoutered.
     mask = np.copy(data)
     mask.fill(0)
     for iy, ix in np.ndindex(data.shape):
@@ -47,8 +53,29 @@ def create_mask(data: npt.NDArray)-> npt.NDArray:
                 mask[x] = 1
                 
     return mask
-    
+
+def find_gears(schematic: npt.NDArray, numbers: npt.NDArray)-> npt.NDArray:
+    # searches for gears and calculate the gear ratios
+    gears = np.copy(schematic)
+    gears.fill(0)
+    for iy, ix in np.ndindex(schematic.shape):
+        if schematic[iy][ix] is not None and schematic[iy][ix]== '*':
+            neighbours_index = calculate_neighbours(iy, ix)
+            neighbours = []
+            # collect neighbours
+            for x in neighbours_index:
+                neighbours.append(numbers[x])
+            # get uniquie values even if two neighbours are the same:
+            # use existing correct_parts method (but only works with 2D)
+            neighbours = correct_parts(np.array([neighbours,neighbours]))[0]
+            neighbours_index = np.nonzero(neighbours)[0]
+            if neighbours_index.size == 2:
+                gears[iy][ix] = neighbours[neighbours_index[0]]*neighbours[neighbours_index[1]]
+
+    return gears
+
 def create_numbers(schema: npt.NDArray)-> npt.NDArray:
+    # aggregate the numbers from the char array
     values = np.zeros(schema.shape, dtype=int)
     number_digits=0
     for iy, ix in np.ndindex(schema.shape):
@@ -80,6 +107,7 @@ def create_numbers(schema: npt.NDArray)-> npt.NDArray:
     return values
 
 def correct_parts(parts: npt.NDArray) ->npt.NDArray:
+    #deletes consecutive duplicated elements
     previous_number = 0
     for iy, ix in np.ndindex(parts.shape):
         if ix == 0:
@@ -104,5 +132,9 @@ if __name__ ==  '__main__':
     #print(mask)
     #print(numbers)
     print(np.sum(parts))
+    
+    gears = find_gears(engine_schematic, numbers)
+    #print(gears)
+    print(np.sum(gears))
     
     
