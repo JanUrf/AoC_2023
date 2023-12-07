d154ce60adbbb6c7b792acac25a4043fbacf9eda
JanUrf <janurfei@gmail.com>
2023-12-07 22:31:06 +0100

    Add solution day7 part 2

--- day7/CamelCards.py
+++ day7/CamelCards.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from enum import Enum
 import numpy as np
 
+
 class Card(Enum):
     TWO = 2
     THREE = 3
@@ -12,10 +13,11 @@ class Card(Enum):
     EIGHT = 8
     NINE = 9
     TEN = 10
-    J = 11
+    J = 1
     Q = 12
     K = 13
     A = 14
+
 
 class CardSet(Enum):
     HIGH_CARD = 0
@@ -26,8 +28,10 @@ class CardSet(Enum):
     FOUR_OF_A_KIND = 5
     FIVE_OF_A_KIND = 6
 
+
 compare_set = lambda hand: hand["set"].value
 highest = lambda hand: [card.value for card in hand["values"]]
+
 
 def parse_input(file_path: Path) -> list:
     data = []
@@ -35,45 +39,67 @@ def parse_input(file_path: Path) -> list:
         for line in f:
             hand = line.rstrip().split()
             card_set = determine_card_set(hand[0])
-            data.append({"bid": int(hand[1]), 
-                             "set": card_set,
-                             "values" : card_values(hand[0])
-                             })
-    return data  
-        
+            data.append(
+                {
+                    "name": hand[0],
+                    "bid": int(hand[1]),
+                    "set": card_set,
+                    "values": card_values(hand[0]),
+                }
+            )
+    return data
+
+
 def determine_card_set(cards: str) -> CardSet:
-    hist, distribution = np.unique(list(cards),return_counts=True)
-    if 5 in distribution:
+    # count occurrence
+    hist, distribution = np.unique(list(cards), return_counts=True)
+    joker_num = 0
+    if "J" in hist:
+        index = hist.tolist().index("J")
+        joker_num = distribution[index]
+        hist = np.delete(hist, index)
+        distribution = np.delete(distribution, index)
+    if 5 - joker_num in distribution:
         return CardSet.FIVE_OF_A_KIND
-    elif 4 in distribution:
+    elif 4 - joker_num in distribution:
         return CardSet.FOUR_OF_A_KIND
-    elif 2 in distribution and 3 in distribution:
+    elif (2 in distribution and 3 in distribution) or (
+        np.count_nonzero(distribution == 2) == 2 and joker_num == 1
+    ):
+        # For the case 1, 2 and two joker four of a kind is better than Full house and handled before
         return CardSet.FULL_HOUSE
-    elif 3 in distribution:
+    elif 3 - joker_num in distribution:
         return CardSet.THREE_OF_A_KIND
-    elif np.count_nonzero(distribution == 2) > 0:
-        # includes one pair and two pair
-        return CardSet(np.count_nonzero(distribution == 2))
+    elif np.count_nonzero(distribution == 2) == 2:
+        return CardSet.TWO_PAIR
+    elif np.count_nonzero(distribution == 2) == 1:
+        return CardSet.ONE_PAIR
     else:
+        if joker_num == 5:
+            return CardSet.FIVE_OF_A_KIND
+        if joker_num == 1:
+            return CardSet.ONE_PAIR
         return CardSet.HIGH_CARD
+
 
 def card_values(hand: str) -> list:
     values = list(hand)
     for i, value in enumerate(values):
         if value.isnumeric():
             values[i] = Card(int(value))
-        elif value == 'T':
+        elif value == "T":
             values[i] = Card.TEN
-        elif value == 'J':
+        elif value == "J":
             values[i] = Card.J
-        elif value == 'Q':
+        elif value == "Q":
             values[i] = Card.Q
-        elif value == 'K':
+        elif value == "K":
             values[i] = Card.K
-        elif value == 'A':
+        elif value == "A":
             values[i] = Card.A
-            
+
     return values
+
 
 if __name__ == "__main__":
     hands = parse_input(Path("day7/input.txt"))
@@ -82,5 +108,5 @@ if __name__ == "__main__":
     # sum up winnings
     winnings = 0
     for i, hand in enumerate(hands):
-        winnings += (i+1) * hand["bid"]
+        winnings += (i + 1) * hand["bid"]
     print("winnings: ", winnings)
