342dcb28742efe889f03aeb2d9605b79a46f8dae
JanUrf <janurfei@gmail.com>
2023-12-08 15:06:40 +0100

    Add solution day8 part 2

--- day8/Wasteland.py
+++ day8/Wasteland.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import re
+import numpy as np
+import math
 
-node_format = re.compile(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)")
+node_format = re.compile(r"(\w{3}) = \((\w{3}), (\w{3})\)")
 
 
 def parse_input(file_path: Path) -> (list, dict):
@@ -17,25 +19,49 @@ def parse_input(file_path: Path) -> (list, dict):
     return navigation, network
 
 
-def walk(start_node: str, target: str, navigation: list, network: dict) -> str:
+def walk(start_node: str, target: list, navigation: list, network: dict) -> str:
     steps = 0
     current_node = start_node
     for step in navigation:
         current_node = network[current_node][step]
         steps += 1
         # if target is reached we can abort
-        if re.match(current_node, target):
+        if current_node in target:
             return current_node, steps
     return current_node, steps
 
 
+def find_end(start: str, navigation: list, network: dict, end_notes: list) -> int:
+    steps = 0
+    current_node = start
+    # Do the steps again until you reach your target
+    while current_node not in end_nodes:
+        current_node, iteration_steps = walk(
+            current_node, end_nodes, navigation, network
+        )
+        steps += iteration_steps
+    print(start, " need ", steps, "steps")
+    return steps
+
+
 if __name__ == "__main__":
     navigation, network = parse_input(Path("day8/input.txt"))
-    steps = 0
-    current_node = "AAA"
-    target = "ZZZ"
-    # Do the steps again until you reach your target
-    while current_node != target:
-        current_node, iteration_steps = walk(current_node, target, navigation, network)
-        steps += iteration_steps
-    print("steps: ", steps)
+
+    start_nodes = list(filter(lambda node: re.match(r"\w{2}A", node), network.keys()))
+    end_nodes = list(filter(lambda node: re.match(r"\w{2}Z", node), network.keys()))
+
+    shortes_way_per_start = [0] * len(start_nodes)
+
+    # find out how long it takes reaching the first from every start node
+    for i, node in enumerate(start_nodes):
+        steps = find_end(node, navigation, network, end_nodes)
+        shortes_way_per_start[i] = steps
+
+    # to synchronize we have to calculate the least common multiple from all nodes
+    # including the len of our navigation series, because if other nodes aren't finished
+    # they'll continue with the series.
+    shortes_way_per_start.append(len(navigation))
+    lcm = math.lcm(*shortes_way_per_start)
+
+    print("steps: ", shortes_way_per_start)
+    print("lcm: ", lcm)
