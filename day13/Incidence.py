6f05bae3ecc9ba141523cd852554fd8166593176
JanUrf <janurfei@gmail.com>
2023-12-13 20:14:38 +0100

    Add solution day13 part 2

--- day13/Incidence.py
+++ day13/Incidence.py
@@ -16,41 +16,58 @@ def parse_input(file_path: Path) -> list:
 
 
 def check_for_horizontal_symmetry(pattern: list, line_number: int) -> bool:
-    first_half = line_number -1
-    second_half = line_number + 2
+    first_half = line_number
+    second_half = line_number + 1
+    pattern = np.array(pattern)
+    correct_smud_already = False
 
-    while 0 <= first_half and second_half < len(pattern):
-        if pattern[first_half] == pattern[second_half]:
-            first_half -= 1
-            second_half += 1
+    while 0 <= first_half and second_half < pattern.shape[0]:
+        differences = (pattern[first_half, :] != pattern[second_half, :]).sum()
+        if not correct_smud_already or differences == 0:
+            if differences == 1:
+                correct_smud_already = True
+
+            if differences <= 1:
+                first_half -= 1
+                second_half += 1
+            else:
+                # not symetrical
+                return False
         else:
-            # not symetrical
             return False
     # One half ended and up to here there were symmetrical.
-    return True
+    return correct_smud_already
 
 
 def check_for_vertical_symmetry(pattern: npt.NDArray, column_number: int) -> bool:
-    first_half = column_number -1
-    second_half = column_number + 2
+    first_half = column_number
+    second_half = column_number + 1
 
+    correct_smud_already = False
     while 0 <= first_half and second_half < pattern[0].shape[0]:
-        if (pattern[:, first_half] == pattern[:, second_half]).all():
-            first_half -= 1
-            second_half += 1
+        differences = (pattern[:, first_half] != pattern[:, second_half]).sum()
+        if not correct_smud_already or differences == 0:
+            if differences == 1:
+                correct_smud_already = True
+            if differences <= 1:
+                first_half -= 1
+                second_half += 1
+            else:
+                # not symetrical
+                return False
+
         else:
-            # not symetrical
             return False
 
     # One half ended and up to here there were symmetrical.
-    return True
+    return correct_smud_already
 
 
 def find_horizontal_symmetry(pattern: list) -> int:
     for i, line in enumerate(pattern):
         # to stop at the second last line
         if i < len(pattern) - 1:
-            if line == pattern[i + 1]:
+            if (np.array(line) != np.array(pattern[i + 1])).sum() <= 1:
                 if check_for_horizontal_symmetry(pattern, i):
                     # in this case we have found the axis
                     return i
@@ -65,7 +82,7 @@ def find_vertical_symmetry(pattern: list) -> int:
     columns = pattern[0].shape[0]
     for i in range(columns):
         if i < columns - 1:
-            if (pattern[:, i] == pattern[:, i + 1]).all():
+            if (pattern[:, i] != pattern[:, i + 1]).sum() <= 1:
                 if check_for_vertical_symmetry(pattern, i):
                     # in this case we have found the axis
                     return i
@@ -85,6 +102,7 @@ if __name__ == "__main__":
         # check for vertical symmetry
         v_axis = find_vertical_symmetry(pattern)
         print("h:", h_axis + 1, "  v:", v_axis + 1)
+
         score += (h_axis + 1) * 100
         score += v_axis + 1
     print("score: ", score)
