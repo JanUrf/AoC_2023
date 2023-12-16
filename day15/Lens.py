c7fbc4260db086726b3817c2364a2fee52af4483
JanUrf <janurfei@gmail.com>
2023-12-16 16:19:39 +0100

    Add solution day 15 part 2

--- day15/Lens.py
+++ day15/Lens.py
@@ -1,10 +1,4 @@
 from pathlib import Path
-import numpy as np
-import numpy.typing as npt
-from enum import Enum
-import sys
-
-np.set_printoptions(threshold=sys.maxsize)
 
 
 def holiday_hash(input: str) -> int:
@@ -22,9 +16,39 @@ def parse_input(file_path: Path) -> list:
         return f.read().rstrip().split(",")
 
 
+def calculate_focusing_power(boxes: list) -> int:
+    focusing_power = 0
+    for box_number, box in enumerate(boxes):
+        slot_number = 1
+        for lense_label in box:
+            focusing_power += (box_number + 1) * slot_number * box[lense_label]
+            slot_number += 1
+
+    return focusing_power
+
+
 if __name__ == "__main__":
     input = parse_input(Path("day15/input.txt"))
-    sum = 0
+    boxes = [{} for _ in range(256)]
+
     for sequence in input:
-        sum += holiday_hash(sequence)
-    print("sum: ", sum)
+        sequence = sequence.split("=")
+        dash = False
+        if len(sequence) == 1:
+            # operator was a '-'
+            dash = True
+            label = sequence[0][:-1]
+        else:
+            label = sequence[0]
+            focal_length = sequence[1]
+
+        box = holiday_hash(label)
+
+        if dash:
+            boxes[box].pop(label, None)
+
+        else:
+            boxes[box][label] = int(focal_length)
+
+    power = calculate_focusing_power(boxes)
+    print("focusing power: ", power)
